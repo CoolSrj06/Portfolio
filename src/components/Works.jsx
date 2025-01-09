314ac0bfcc308c3110acb6b39c54c6216133419f
CoolSrj06 <srijanmaurya6602@gmail.com>
2025-01-09 12:46:24 +0530

    Add Basic Chat Application to Works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import UnitStorage from "../assets/UnitStorage.png";
 import CodeWhispers from "../assets/CodeWhispers.png";
+import BasicChatApp from "../assets/Basic Chat Application.png";
 import { FaGithub, FaLink } from "react-icons/fa6";
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -114,6 +115,39 @@ export const Works = () => {
               <span className="mt-3 mx-2 pt-2 border-t border-[#0C0C0C] flex items-center justify-between">
                 <Link to="https://github.com/CoolSrj06/YouTube-Time-Calculator-Extension.git">
                   <FaGithub className="text-lg" />
+                </Link>
+              </span>
+            </div>
+            <div className="works-container" data-aos="fade-up">
+              <img
+                src={BasicChatApp}
+                alt="BasicChatApp-img"
+                className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
+              />
+              <div>
+                <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                  Simple Chat Application
+                </h1>
+                <p className="mb-3 px-2 text-sm md:text-base">
+                  This is a simple chat application that operates on websockets. It is a 
+                  global platform that allows you to interact with others while anonmous.
+                </p>
+                <span className="flex flex-wrap gap-2 px-2">
+                  <p className="works-pill">HTML</p>
+                  <p className="works-pill">CSS</p>
+                  <p className="works-pill">Javascript</p>
+                  <p className="works-pill">Node.js</p>
+                  <p className="works-pill">Express.js</p>
+                  <p className="works-pill">Websockets</p>
+                  <p className="works-pill">SQLite</p>
+                </span>
+              </div>
+              <span className="mt-3 mx-2 pt-2 border-t border-[#0C0C0C] flex items-center justify-between">
+                <Link to="https://github.com/CoolSrj06/Basic-Chat-Application.git">
+                  <FaGithub className="text-lg" />
+                </Link>
+                <Link to="https://basic-chat-application-6728.onrender.com">
+                  <FaLink className="text-lg" />
                 </Link>
               </span>
             </div>
